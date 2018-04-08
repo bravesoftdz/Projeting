f954026db34f13c963bfbc0a12421d35873162ed
SAID Abdelhamid <said.abdelhamid.94@gmail.com>
2018-04-08 01:37:22 +0100

    intégration du header

---
+++ Dev/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+
+		<!-- Intégration bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<!-- Font Roboto -->
+		<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+		<!-- Mon CSS -->
+		<link rel="stylesheet" href="assets/css/style.css">
+		<link rel="stylesheet" href="sign/assets/css/material-kit.css">
+
+
+	<title>Home page</title>
+</head>
+<body>
+		<header class="container-fluid">
+			<div class="row">
+				<div class="col-1">
+
+				</div>
+				<div class="col-2 offset">
+					<img src="img/LOGO.png" alt="" class="logo">
+				</div>
+				<ul class="col-2">
+					<li><a href="#">Parcours</a></li>
+					<li><a href="#">Cours</a></li>
+				</ul>
+				<div class="col-3">
+	    				<input type="text" value="" placeholder="" class="" />
+				</div>
+				<div class="col-2">
+						<button class="btn btn-info sins">s'inscrire</button>
+				</div>
+				<div class="col-2">
+						<button class="btn btn-white seco">se connecter</button>
+				</div>
+			</div>
+		</header>
+		<section class="loool">
+			<p>
+				<h1>Faites décoller votre carrière</h1>
+				<h2>Nos parcours 100% en ligne sont diplômants et vous garantissent un emploi.</h2>
+			</p>
+		</section>
+
+
+
+
+</body>
+
+
+
+
+
+
+
+
+
+
+<script src="sign/assets/js/jquery.min.js" type="text/javascript"></script>
+<script src="sign/assets/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="sign/assets/js/material.min.js"></script>
+
+<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/-->
+<script src="sign/assets/js/nouislider.min.js" type="text/javascript"></script>
+<!--  Plugin for the Datepicker, full documentation here: http://www.eyecon.ro/bootstrap-datepicker/-->
+<script src="sign/assets/js/bootstrap-datepicker.js" type="text/javascript"></script>
+
+<!-- Control Center for Material Kit: activating the ripples, parallax effects, scripts from the example pages etc-->
+<script src="sign/assets/js/material-kit.js" type="text/javascript"></script>
+
+
+</html>
